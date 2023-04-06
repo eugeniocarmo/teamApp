2434a8ab38cddda1de50e70c03dac60f15dca829
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-04-06 22:37:35 +0100

    feat: "first commit - teamApp started"

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>TeamApp development starts</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create ({
+  container : {
+    flex : 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textdeco : {
+    color: '#fff',
+  },
+})
