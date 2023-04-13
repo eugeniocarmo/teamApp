c65a828b6847bdc67275c612c69e0a4078d0d9c5
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-04-13 21:29:41 +0100

    feat: "Installed Roboto google fonts"

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from 'styled-components';
+import { ActivityIndicator } from 'react-native/';
 import theme from './src/theme';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Groups } from '@screens/Groups';
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular,Roboto_700Bold,});
+
   return (
     <ThemeProvider theme={ theme }>
-      <Groups />
+      { fontsLoaded ? <Groups />: <ActivityIndicator /> }
     </ThemeProvider>
   );      
 }
