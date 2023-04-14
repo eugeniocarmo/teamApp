bb02e3d5367a810d6716d66114addd3803b8b5c7
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-04-14 13:55:24 +0100

    feat: "Created Loading Component"

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import { ThemeProvider } from 'styled-components';
 import { ActivityIndicator } from 'react-native/';
-import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Loading } from '@components/Loading'
+import theme from './src/theme';
 import { Groups } from '@screens/Groups';
 
 
@@ -11,7 +12,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={ theme }>
-      { fontsLoaded ? <Groups />: <ActivityIndicator /> }
+      { fontsLoaded ? <Groups />: <Loading /> }
     </ThemeProvider>
   );      
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+
+import { Container, LoadIndicator } from "./styles";
+
+export function Loading() {
+
+  return (
+    <Container>
+      <LoadIndicator color="red" /> 
+    </Container>
+
+  )
+
+};
