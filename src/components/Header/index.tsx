dac1925d4f27b0014d531cf18afe63568dcb451e
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-04-15 22:45:57 +0100

    feat: "Adding Phosphor Icons library"

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
-import { Container, Logo } from './styles';
-
+import { BackIcon, Container, Logo, BackButton} from './styles';
 import ImagesImg from '@assets/logo.png';
 
 export function Header() {
 
   return (
     <Container>
+      <BackButton>
+        <BackIcon/>
+      </BackButton>      
+
       <Logo source={ ImagesImg }/>
     </Container>
   )
