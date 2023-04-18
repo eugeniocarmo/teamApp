a4185fd1f68988af00cf2541ae546633580d6a31
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-04-18 19:00:46 +0100

    Highlight component created

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,16 @@
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
+
 
 
 export function Groups() {
   return (
     <Container>
       <Header/>
+      <Highlight 
+        title='TeamUp' 
+        subtitle='Create your team, challenge your friends, and dominate the competition'/>
     </Container>
   );
 }
