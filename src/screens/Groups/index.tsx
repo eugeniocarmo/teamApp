9dd17490240766e8b7be6cd5d878fc424bd45d58
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-04-20 21:21:34 +0100

    feat:"Listing Groups"

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
@@ -7,15 +9,24 @@ import { Container } from './styles';
 
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['A Team', 'Team GB', 'Team 3', '4Blue Team', '5 Guys', '6Sense ']);
+
   return (
     <Container>
       <Header/>
       <Highlight 
         title='TeamUp' 
         subtitle='Create your team, challenge your friends, and dominate the competition'/>
-      <GroupCard
-        title='Super Group' 
+ 
+      <FlatList 
+        data={groups}
+        keyExtractor = { item => item }
+        renderItem = {({ item }) => (
+          <GroupCard 
+            title={ item }
+          />
+        )}
       />
     </Container>
-  );
+  ); 
 }
