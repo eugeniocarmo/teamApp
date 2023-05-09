283bc76d1277c50026d38af3b14e48b4e8383419
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-05-09 15:15:53 +0100

    feat: Preventing app glitches

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
+import { useTheme } from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './app.routes';
+import { View } from 'react-native';
 
-export function Routes(){
-  return(
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+export function Routes() {
+  const { COLORS } = useTheme();
 
+  return (
+    <View style={{ flex:1, backgroundColor: COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   );
-
 }
 
